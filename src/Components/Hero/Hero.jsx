b3ff2ab06ab6c743de41b5d1bcbd5cc37fbf9cd9
glitch-samson <glitchsamson375@gmail.com>
2025-04-10 04:34:15 +0100

    Your commit message

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../../Assets/hand_icon.png'
 import arrow_icon from '../../Assets/arrow.png'
-import hero_image from '../../Assets/hero_image.png'
+import product_20 from '../../Assets/product_20.png'
 
 const Hero = () => {
   return (
@@ -23,7 +23,7 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero-right">
-        <img src={hero_image} alt="" />
+        <img src={product_20} alt="" />
       </div>
     </div>
   )
